98da999f33893bc55066011630840c88bccc0dc5
Shiva pandey <107467698+Shivastoic@users.noreply.github.com>
2024-03-26 16:41:41 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,194 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * NAVBAR
+ * navbar toggle for mobile
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+// addEventOnElements(navTogglers, "click", toggleNavbar);
+
+navTogglers.forEach(toggler => {
+  toggler.addEventListener("click", toggleNavbar);
+});
+
+// Add event listener to close navbar when a navigation link is clicked
+navbar.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", toggleNavbar);
+});
+
+
+/**
+ * HEADER
+ * header active when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function (currentSlider) {
+
+  const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+  let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  let currentSlidePos = 0;
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  const moveSliderItem = function () {
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+  }
+
+  /**
+   * NEXT SLIDE
+   */
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= totalSlidableItems;
+
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /**
+   * PREVIOUS SLIDE
+   */
+  const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+      currentSlidePos = totalSlidableItems;
+    } else {
+      currentSlidePos--;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = totalSlidableItems <= 0;
+  if (dontHaveExtraItem) {
+    sliderNextBtn.style.display = 'none';
+    sliderPrevBtn.style.display = 'none';
+  }
+
+  /**
+   * slide with [shift + mouse wheel] or touch
+   */
+  currentSlider.addEventListener("wheel", function (event) {
+    if (event.shiftKey && event.deltaY > 0) slideNext();
+    if (event.shiftKey && event.deltaY < 0) slidePrev();
+  });
+
+  currentSlider.addEventListener("touchstart", function (event) {
+    touchStartX = event.touches[0].clientX;
+  });
+
+  currentSlider.addEventListener("touchmove", function (event) {
+    touchEndX = event.touches[0].clientX;
+  });
+
+  currentSlider.addEventListener("touchend", function (event) {
+    const touchDiff = touchStartX - touchEndX;
+    if (touchDiff > 0) {
+      slideNext();
+    } else if (touchDiff < 0) {
+      slidePrev();
+    }
+    touchStartX = 0;
+    touchEndX = 0;
+  });
+
+
+
+  /**
+   * RESPONSIVE
+   */
+
+  window.addEventListener("resize", function () {
+    totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    moveSliderItem();
+  });
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
+
+
+
+// Initialize ScrollReveal
+ScrollReveal().reveal('.scroll-reveal-left', {
+  delay: 800,
+  distance: '50px',
+  duration: 900,
+  easing: 'ease-in-out',
+  origin: 'left',
+  reset: true,
+  mobile: true
+});
+
+
